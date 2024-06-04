c49a5ab001ebe439e919c17472bf9152543c8ee3
webdoli <topofweird@gmail.com>
2024-06-04 12:06:46 +0900

    1.1.9 update

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,17 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'src/mogl3d-editor.module.js', // 진입점 파일 경로
-    output: [
-        {
-            file: 'lib/mogl3d-editor.module.js',
-            format: 'es',
-        },
-        {
-            file: 'lib/mogl3d-editor.js',
-            format: 'cjs',
-            exports: 'default',
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        terser() // 코드를 최소화하여 최적화
-    ]
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/mogl3d-editor.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/mogl3d-editor.module.js',
+      format: 'es'
+    }
+  ],
+  plugins: [resolve(), commonjs()]
 };
